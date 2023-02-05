efc0a3e92128e4c08273310f59a0e777303a7bbc
Frank Liu <frankliu81@gmail.com>
2023-02-05 10:38:19 -0800

    basic lambda apigateway

---
+++ services/service2/test/service2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Service2 from '../lib/service2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/service2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Service2.Service2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
