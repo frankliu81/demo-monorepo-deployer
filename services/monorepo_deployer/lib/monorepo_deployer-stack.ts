b1c1c47b0a6bfccc872a742ac7af2a7066d777c7
Frank Liu <frankliu81@gmail.com>
2023-03-31 14:30:43 -0700

    add permission for lambda to trigger service1 codepipline

--- services/monorepo_deployer/lib/monorepo_deployer-stack.ts
+++ services/monorepo_deployer/lib/monorepo_deployer-stack.ts
@@ -7,29 +7,41 @@ import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import * as ssm from "aws-cdk-lib/aws-ssm";
-//import * as iam from 'aws-cdk-lib/aws-iam';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 
 export class MonorepoDeployerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const service1PipelineName = ssm.StringParameter.valueForStringParameter(
+      this,
+      `/service1/pipeline_stack/codepipeline_name`);
+
     // The code that defines your stack goes here
     const lambdaFunction = new NodejsFunction(this, "frank-demo-monorepo-deployer", {
       runtime: Runtime.NODEJS_16_X,
       entry: path.join(__dirname, "../src/index.ts"),
       handler: 'handler', // this string should match the exports in lambda
-      bundling: {
-        externalModules: ['aws-sdk']
-      },
-      
+      // bundling: {
+      //   externalModules: ['aws-sdk'] // this was for AWS SDK v2 which is included in Node 16, 
+                                        // we will use AWS SDK v3, and have to bundle it.
+      // },
       environment: {
         CDK_SCOPE: process.env.CDK_SCOPE!,
-        SERVICE1_CODEPIPELINE_NAME: ssm.StringParameter.valueForStringParameter(
-          this,
-          `/service1/pipeline_stack/codepipeline_name`,
-        )
-      }
+        SERVICE1_CODEPIPELINE_NAME: service1PipelineName
+      },
+      initialPolicy: [
+        // permission for lambda to execute codepipelin
+        new PolicyStatement({
+          actions: [
+            'codepipeline:*',
+          ],
+          resources: [
+            '*'
+          ]
+        })
+      ]
     });
 
     const api = new LambdaRestApi(this, "frank-demo-monorepo-deployer-api", {
@@ -43,9 +55,6 @@ export class MonorepoDeployerStack extends cdk.Stack {
       encryptionKey: new kms.Key(this, 's3BucketKMSKey'),
     });
 
-    //s3Bucket.grantRead(new iam.AccountRootPrincipal());
     s3Bucket.grantRead(lambdaFunction)
-
-
   }
 }
--- services/monorepo_deployer/src/index.ts
+++ services/monorepo_deployer/src/index.ts
@@ -1,25 +1,39 @@
 
+import { CodePipelineClient, AcknowledgeJobCommand, StartPipelineExecutionCommand } from "@aws-sdk/client-codepipeline";
 
 export const handler = async (event: any, context: any, callback: any) => {
   console.log("CDK_SCOPE " + process.env.CDK_SCOPE)
-  console.log("SERVICE1_CODEPIPELINE_NAME " + process.env.SERVICE1_CODEPIPELINE_NAME)
   console.log("event " + JSON.stringify(event, null, 2))
-  console.log("parse event.body" + JSON.stringify(JSON.parse(event.body)))
-  const name = JSON.parse(event.body)['repository']['name']
-  console.log("parse respository name " + name)
-  
-  if (name === 'jobi') {
-    return {
-      statusCode: 200,
-      headers: {},
-      body: "skipped"
-    };
-  }
+  console.log("SERVICE1_CODEPIPELINE_NAME " + process.env.SERVICE1_CODEPIPELINE_NAME)
 
-  if (name == 'demo-monorepo-deployer')
+
+  if (event.body)
   {
+    console.log("parse event.body" + JSON.stringify(JSON.parse(event.body)))
+    const name = JSON.parse(event.body)['repository']['name']
+    console.log("parse respository name " + name)
 
+    if (name === 'jobi') {
+      return {
+        statusCode: 200,
+        headers: {},
+        body: "skipped"
+      };
+    }
+
+    if (name == 'demo-monorepo-deployer')
+    {
+    }
   }
+
+  // TESTING - lambda triggering pipeline directly without github event
+  const client = new CodePipelineClient({ region: "ca-central-1" });
+  const input = { // StartPipelineExecutionInput
+    name: process.env.SERVICE1_CODEPIPELINE_NAME, // required
+    // clientRequestToken: "STRING_VALUE",
+  };
+  const command = new StartPipelineExecutionCommand(input);
+  const response = await client.send(command);
 
   return {
     statusCode: 200,
